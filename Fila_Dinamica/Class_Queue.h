4a30a126b32c502ac4c94235e1d1d178e8b39823
Marcus Vinicius <marcusviny63@gmail.com>
2023-04-10 01:27:06 -0300

    Algumas mudanças nos métodos

--- Fila_Dinamica/Class_Queue.h
+++ Fila_Dinamica/Class_Queue.h
@@ -2,36 +2,118 @@
 #include "Class_Node.h"
 using namespace std;
 
-//Criando os metodos Node
-template <typename Trem> Node<Trem> * Node<Trem>::MontaNode(Trem dat){
-    Node * P = new Node;
-    if(P){
-        P -> D = dat;
-        P -> Next = NULL;
+// Criando os metodos Node
+template <typename Trem>
+Node<Trem> *Node<Trem>::MontaNode(Trem dat)
+{
+    Node *P = new Node;
+    if (P)
+    {
+        P->D = dat;
+        P->Next = NULL;
     }
     return P;
 }
-template <typename Trem> Trem Node<Trem>::DesmontaNode(Node * P){
+template <typename Trem>
+Trem Node<Trem>::DesmontaNode(Node *P)
+{
     Trem X;
-    if(P){
-        X = P -> D;
+    if (P)
+    {
+        X = P->D;
         delete P;
     }
     return X;
 }
 
-//Criando a Class Queue
-template <typename Trem> class Queue{
-    public:
-    Node<Trem> * head;
-    Node<Trem>* tail;
+// Criando a Class Queue
+template <typename Trem>
+class Queue
+{
+public:
+    Node<Trem> *head;
+    Node<Trem> *tail;
     int N;
-    Queue();
-    ~Queue();
-    bool push(Trem x);
-    Trem front();
-    Trem pop();
-    int size();
-    bool empty();
-    void clear();
+    Queue()
+    {
+        head = tail = NULL;
+        N = 0;
+    }
+    ~Queue()
+    {
+        Node<Trem> *P = head;
+        while (head)
+        {
+            head = P->Next;
+            Node<Trem>::DesmontaNode(P);
+            P = head;
+        }
+    }
+    bool push(Trem x)
+    {
+        Node<Trem> *P = Node<Trem>::MontaNode(x);
+        if (!P)
+        {
+            return false;
+        }
+        if (head)
+        {
+            tail->Next = P;
+        }
+        else
+        {
+            head = P;
+        }
+        tail = P;
+        N++;
+        return true;
+    }
+    Trem front()
+    {
+        Trem x;
+        if (head)
+        {
+            x = head->D;
+        }
+        return x;
+    }
+    Trem pop()
+    {
+        Trem x;
+        if (head)
+        {
+            Node<Trem> *P = head;
+            head = head->Next;
+            x = Node<Trem>::DesmontaNode(P);
+            if (!head)
+            {
+                tail = NULL;
+            }
+            else
+            {
+                tail->Next = head;
+            }
+            N--;
+        }
+        return x;
+    }
+    int size()
+    {
+        return N;
+    }
+    bool empty()
+    {
+        return !head;
+    }
+    void clear()
+    {
+        Node<Trem> *P = head;
+        while (head)
+        {
+            head = P->Next;
+            Node<Trem>::DesmontaNode(P);
+            P = head;
+        }
+        tail = N = NULL;
+    }
 };
--- Pilha_Dinamica/Class_Stack.h
+++ Pilha_Dinamica/Class_Stack.h
@@ -1,33 +1,90 @@
 #include "Class_Node.h"
 
-//Criando os metodos Node
-template <typename Trem> Node<Trem> * Node<Trem>::MontaNode(Trem dat){
-    Node<Trem> * P = new Node<Trem>;
-    if(P){
-        P -> D = dat;
-        P -> Next = NULL;
+// Criando os metodos Node
+template <typename Trem>
+Node<Trem> *Node<Trem>::MontaNode(Trem dat)
+{
+    Node<Trem> *P = new Node<Trem>;
+    if (P)
+    {
+        P->D = dat;
+        P->Next = NULL;
     }
     return P;
 }
-template <typename Trem> Trem Node<Trem>::DesmontaNode(Node<Trem> * P){
+template <typename Trem>
+Trem Node<Trem>::DesmontaNode(Node<Trem> *P)
+{
     Trem X;
-    if(P){
-        X = P -> D;
+    if (P)
+    {
+        X = P->D;
         delete P;
     }
     return X;
 }
 
-//Classe Stack
-template <typename Trem> class Stack{
-    public:
-        Node<Trem> * Top;
-        int N;
-        Stack();
-        ~Stack();
-        bool push(Trem X);
-        Trem top();
-        Trem pop();
-        int size();
-        bool empty();
+// Classe Stack
+template <typename Trem>
+class Stack
+{
+public:
+    Node<Trem> *Top;
+    int N;
+    Stack()
+    {
+        Top = NULL;
+        N = 0;
+    }
+    ~Stack()
+    {
+        Node<Trem> *P;
+        while (Top)
+        {
+            P = Top;
+            Top = Top->Next;
+            Node<Trem>::DesmontaNode(P);
+        }
+    }
+    bool push(Trem X)
+    {
+        Node<Trem> *P = Node<Trem>::MontaNode(X);
+        if (!P)
+        {
+            return false;
+        }
+        P->Next = Top;
+        Top = P;
+        N++;
+        return true;
+    }
+    Trem top()
+    {
+        Trem X;
+        if (Top)
+        {
+            X = Top->D;
+        }
+        return X;
+    }
+    Trem pop()
+    {
+        Trem X;
+        if (Top)
+        {
+            Node<Trem> *P = Top;
+            Top = Top->Next;
+            X = Node<Trem>::DesmontaNode(P);
+            N--;
+        }
+        return X;
+    }
+    int size()
+    {
+        return N;
+    }
+    bool empty()
+    {
+        return !Top;
+    }
 };
